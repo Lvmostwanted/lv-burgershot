1d7ea9419f68b3cabc2869a7d721d82df0c438f3
Sean Spirins <48731374+Lvmostwanted@users.noreply.github.com>
2023-03-22 23:53:59 +0000

    Page routing

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import Router from 'svelte-spa-router';
+    import routes from './routes';
     import VisibilityProvider from "./providers/VisibilityProvider.svelte";
     import HelloWorld from "./components/HelloWorld.svelte";
     import Tailwind from "./Tailwind.svelte";
@@ -8,5 +10,6 @@
     <Tailwind />
     <VisibilityProvider>
         <HelloWorld />
+        <Router {routes} />
     </VisibilityProvider>
 </main>
---
+++ web/src/routes.ts
@@ -0,0 +1,7 @@
+import LandingPage from './routes/LandingPage.svelte';
+import Test from './routes/Test.svelte';
+
+export default {
+    '/': LandingPage,
+    '/test': Test,
+};
