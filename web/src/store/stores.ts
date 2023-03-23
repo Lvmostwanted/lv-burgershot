7e22bde1f3c567cd3a0c785c4634cb03ad19a090
Sean Spirins <48731374+Lvmostwanted@users.noreply.github.com>
2023-03-23 00:39:06 +0000

    (Reminder) visibility store

--- web/src/store/stores.ts
+++ web/src/store/stores.ts
@@ -1,3 +1,7 @@
 import { writable } from "svelte/store";
 
+// Change this value to false before
+// production use to ensure that the
+// NUI does not appear without usage
+// of the appropriate events
 export const visibility = writable(true);
